8466d017b66fbd7761127ccf88ad1c961210f88b
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-11-14 10:10:02 -0800

    add Student

---
+++ src/lmmutable/Test.java
@@ -0,0 +1,9 @@
+package lmmutable;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student(111223333, "john");
+        java.util.Date dateCreated = student.getDateCreated();
+        dateCreated.setTime(200000);
+    }
+}
