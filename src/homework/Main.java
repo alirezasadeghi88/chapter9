9edfa91316971d3f76fe9236d91c4ae20858f06c
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-11-14 04:34:10 -0800

    add object

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -1,72 +1,81 @@
 package homework;
 
 public class Main {
-    public static void main(String[] args) {
-      Human ali = new Human();
-        Human.count++ ;
-        ali.age = 16;
-        ali.wright = 70;
-        ali.height = 184;
-        ali.gender = "boy";
+      public static void main(String[] args) {
+//      Human ali = new Human();
+//        Human.count++ ;
+//        ali.age = 16;
+//        ali.wright = 70;
+//        ali.height = 184;
+//        ali.gender = "boy";
+//
+//        System.out.println("ALI");
+//
+//        System.out.println(Human.count);
+//        System.out.println(ali.age);
+//        System.out.println(ali.height);
+//        System.out.println(ali.wright);
+//        System.out.println(ali.gender);
+//
+//        ali.walk();
+//        ali.drink();
+//        ali.eat();
+//        ali.sit();
+//        System.out.println("_________________");
+//        System.out.println("REZA");
+//
+//      Human reza = new Human();
+//
+//        Human.count++;
+//        reza.age = 20;
+//        reza.wright = 80;
+//        reza.height = 170;
+//        reza.gender = "boy";
+//
+//        System.out.println(Human.count);
+//        System.out.println(reza.age);
+//        System.out.println(reza.height);
+//        System.out.println(reza.wright);
+//        System.out.println(reza.gender);
+//
+//        reza.walk();
+//        reza.drink();
+//        reza.eat();
+//        reza.sit();
+//        System.out.println("_________________");
+//        System.out.println("ZAHRA");
+//
+//        Human zahra = new Human();
+//        Human.count++;
+//        zahra.age = 23;
+//        zahra.wright = 60;
+//        zahra.height = 168;
+//        zahra.gender = "girl";
+//
+//        System.out.println(Human.count);
+//        System.out.println(zahra.age);
+//        System.out.println(zahra.height);
+//        System.out.println(zahra.wright);
+//        System.out.println(zahra.gender);
+//
+//        zahra.walk();
+//        zahra.drink();
+//        zahra.eat();
+//        zahra.sit();
+//        System.out.println("_________________");
+//
+//
+          Motorcycle honda = new Motorcycle();
+          honda.color = "red";
+          honda.size = 150;
+          honda.type = "125 cc";
 
-        System.out.println("ALI");
+          System.out.println(honda.color);
+          System.out.println(honda.size);
+          System.out.println(honda.type);
 
-        System.out.println(Human.count);
-        System.out.println(ali.age);
-        System.out.println(ali.height);
-        System.out.println(ali.wright);
-        System.out.println(ali.gender);
-
-        ali.walk();
-        ali.drink();
-        ali.eat();
-        ali.sit();
-        System.out.println("_________________");
-        System.out.println("REZA");
-
-      Human reza = new Human();
-
-        Human.count++;
-        reza.age = 20;
-        reza.wright = 80;
-        reza.height = 170;
-        reza.gender = "boy";
-
-        System.out.println(Human.count);
-        System.out.println(reza.age);
-        System.out.println(reza.height);
-        System.out.println(reza.wright);
-        System.out.println(reza.gender);
-
-        reza.walk();
-        reza.drink();
-        reza.eat();
-        reza.sit();
-        System.out.println("_________________");
-        System.out.println("ZAHRA");
-
-        Human zahra = new Human();
-        Human.count++;
-        zahra.age = 23;
-        zahra.wright = 60;
-        zahra.height = 168;
-        zahra.gender = "girl";
-
-        System.out.println(Human.count);
-        System.out.println(zahra.age);
-        System.out.println(zahra.height);
-        System.out.println(zahra.wright);
-        System.out.println(zahra.gender);
-
-        zahra.walk();
-        zahra.drink();
-        zahra.eat();
-        zahra.sit();
-        System.out.println("_________________");
-
-
-    }
-
-
-
+          honda.start();
+          honda.gear();
+          honda.brakes();
+      }
 }
---
+++ src/homework/Motorcycle.java
@@ -0,0 +1,19 @@
+package homework;
+
+public class Motorcycle {
+    public int size;
+    public String color;
+    public String type;
+
+    public void start() {
+        System.out.println("Starter to start the motorcycle.");
+    }
+
+    public void gear() {
+        System.out.println("Gear for starting the motorcycle.");
+    }
+
+    public void brakes() {
+        System.out.println("Brakes to hold the motorcycle.");
+    }
+}
