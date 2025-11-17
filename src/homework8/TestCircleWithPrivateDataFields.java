72c7ea6399f6720918f032333cb61c1c2e024eeb
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-11-17 08:10:41 -0800

    add Circle

---
+++ src/homework8/TestCircleWithPrivateDataFields.java
@@ -0,0 +1,17 @@
+package homework8;
+
+public class TestCircleWithPrivateDataFields {
+
+    public static void main(String[] args) {
+         Circle myCircle = new Circle();
+        System.out.println("The area of the circle of radius "
+            + myCircle.getRadius() + " is " + myCircle.getArea());
+
+        myCircle.setRadius(myCircle.getRadius() * 1.1);
+        System.out.println("The area of the circle of radius "
+            + myCircle.getRadius() + " is " + myCircle.getArea());
+
+        System.out.println("The number of objects created is "
+            + Circle.getNumberOfObjects());
+    }
+}
