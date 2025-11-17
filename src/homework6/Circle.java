fe46577e6adc0b73b72911b9beed2f141b88e916
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-11-17 07:07:16 -0800

    add Circle

---
+++ src/homework6/Circle.java
@@ -0,0 +1,25 @@
+package homework6;
+
+ class Circle {
+
+     double radius = 1;
+
+     Circle() {
+     }
+
+     Circle(double newRadius) {
+         radius = newRadius;
+     }
+
+     double getArea() {
+         return radius * radius * Math.PI;
+     }
+
+     double getPerimeter() {
+         return 2 * radius * Math.PI;
+     }
+
+     void setRadius(double newRadius) {
+         radius = newRadius;
+     }
+}
